10ef75fae7fd6b2d0c22885c32c30456a5b431e7
Anne <hiuhin@gmail.com>
2020-06-03 14:17:07 -0700

    Add Card Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from "react-bootstrap/Card";
-import CardDeck from "react-bootstrap/CardDeck";
+import {CardDeck} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
+import CardComponent from "./components/CardComponent";
+import Columns from "react-columns";
 
 function App() {
+  const [totalStats, setTotalStats] = useState([]);
+  const [byCountries, setByCountries] = useState([]);
+  useEffect(() => {
+    axios
+      .all([
+        axios.get("https://disease.sh/v2/all"),
+        axios.get("https://disease.sh/v2/countries"),
+      ])
+      .then((res) => {
+        setTotalStats(res[0].data);
+        setByCountries(res[1].data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const lastUpdatedDate = new Date(totalStats.updated).toString();
+  const queries = [{
+    columns: 2,
+    query: 'min-width: 500px'
+  }, {
+    columns: 5,
+    query: 'min-width: 1000px'
+  }];
+
   return (
     <div className="App">
       <CardDeck>
@@ -16,10 +44,10 @@ function App() {
         >
           <Card.Body>
             <Card.Title>Cases</Card.Title>
-            <Card.Text>100</Card.Text>
+            <Card.Text>{totalStats.cases}</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small>Last updated 3 mins ago</small>
+            <small>Last Updated {lastUpdatedDate}</small>
           </Card.Footer>
         </Card>
         <Card
@@ -30,10 +58,10 @@ function App() {
         >
           <Card.Body>
             <Card.Title>Deaths</Card.Title>
-            <Card.Text>0</Card.Text>
+            <Card.Text>{totalStats.deaths}</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small>Last Updated {lastUpdatedDate}</small>
           </Card.Footer>
         </Card>
         <Card
@@ -44,13 +72,22 @@ function App() {
         >
           <Card.Body>
             <Card.Title>Recovered</Card.Title>
-            <Card.Text>99</Card.Text>
+            <Card.Text>{totalStats.recovered}</Card.Text>
           </Card.Body>
           <Card.Footer>
-            <small className="text-muted">Last updated 3 mins ago</small>
+            <small>Last updated {lastUpdatedDate}</small>
           </Card.Footer>
         </Card>
       </CardDeck>
+        <Columns queries={queries}>
+          {byCountries.map((country, i) => (
+            <CardComponent
+              key={i}
+              country={country}
+              style={{ margin: "10px" }}
+            />
+          ))}
+        </Columns>
     </div>
   );
 }
---
+++ src/components/CardComponent.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Card from "react-bootstrap/Card";
+
+export default function CardComponent(props) {
+    const country = props.country;
+
+    return (
+      <Card style={{ margin: "10px" }}>
+        <Card.Img variant="top" src={country.countryInfo.flag} />
+        <Card.Body>
+          <Card.Title className="text-center">{country.country}</Card.Title>
+          <Card.Text>Cases: {country.cases}</Card.Text>
+          <Card.Text>Deaths: {country.deaths}</Card.Text>
+          <Card.Text>Recovered: {country.recovered}</Card.Text>
+          <Card.Text>Active: {country.active}</Card.Text>
+          <Card.Text>Critical: {country.critical}</Card.Text>
+        </Card.Body>
+      </Card>
+    );
+}
